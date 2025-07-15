4d36a2d66827a9ed0cdbd57102af0fcd97e82bc6
Davi Henrique Lima Carvalho <davihenriquelcdev@gmail.com>
2025-07-14 21:16:14 -0300

    criando API para imagens,adicionando arquivo com algumas imagens

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,30 +1,27 @@
-import React from "react";
-import'./ProductCard.css';
+import React from 'react';
+import './ProductCard.css';
 import propTypes from 'prop-types';
-import AddNoCarrinhoBtn from '../AddNoCarrinhoBtn/AddNoCarrinhoBtn'
-function ProductCard({ data }){
-    
-    const {title, thumbnail, price, description} = data
+import AddNoCarrinhoBtn from '../AddNoCarrinhoBtn/AddNoCarrinhoBtn';
+function ProductCard({ data }) {
+  const { title, thumbnail, price, description } = data;
 
-    return(
-        <section className="product-card">
-            <img src={thumbnail} 
-            className="card__image"/>
+  return (
+    <section className="product-card">
+      <img src={thumbnail} className="card__image" />
 
-            <div className="card__infos">
-                <h2 className="card__title">{title}</h2>
-                <h2 className="card__price">{price}</h2>
-                <h3 className="card__description">{description}</h3>
-            </div>
+      <div className="card__infos">
+        <h2 className="card__title">{title}</h2>
+        <h2 className="card__price">{price}</h2>
+        <h3 className="card__description">{description}</h3>
+      </div>
 
-            <AddNoCarrinhoBtn />
-
-        </section>
-    );
+      <AddNoCarrinhoBtn />
+    </section>
+  );
 }
 
 export default ProductCard;
 
 ProductCard.propTypes = {
-    data: propTypes.shape({}),
+  data: propTypes.shape({}),
 }.isRequired;
