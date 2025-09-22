98985fb5c55015bb9dbbba870974645a77143cf8
Davi Henrique Lima Carvalho <davihenriquelcdev@gmail.com>
2025-09-22 18:27:28 -0300

    Ajustando Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import '../Header/header.css'
+import '../Header/header.css';
 import CarrinhoBtn from '../CarrinhoBtn/CarrinhoBtn';
 import { body } from 'framer-motion/client';
 
@@ -7,11 +7,11 @@ function Header() {
   return (
     <div className="header">
       <h2 className="logo">Logo</h2>
-      <nav className='navigation'>
-        <a href="#">Home</a>
-        <a href="#">Produtos</a>
-        <a href="#">Carrinho</a>
-        <button className='btnLogin'>Login</button>
+      <nav className="navigation">
+        <Link to="/">Home</Link>
+        <Link to="/produtos">Produtos</Link>
+        <Link to="/carrinho">Carrinho</Link>
+        <Link to="/login" className="btnLogin">Login</Link>
       </nav>
     </div>
   );
--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import CheckoutCarrinho from './pages/CheckoutCarrinho/CheckoutCarrinho';
 import App from './App';
 import ProdutosPage from './pages/Produtos/ProdutosPage';
 import Home from './pages/Home.jsx'
+import Login from './pages/Login/login.jsx'
 
 let router = createBrowserRouter([
   {
@@ -17,6 +18,10 @@ let router = createBrowserRouter([
     path: '/checkout',
     element: <CheckoutCarrinho />,
   },
+  {
+  path: '/login',
+  element: <Login />,
+  }
 ]);
 
 export default router;
