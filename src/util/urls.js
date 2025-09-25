709efc3544f69be01917c81bc412cf3348f7787f
luisfr364 <luisfr364@gmail.com>
2025-09-25 01:16:08 -0300

    fix: update commented localhost apiUrl for consistency

--- src/util/urls.js
+++ src/util/urls.js
@@ -2,4 +2,4 @@ const apiUrl =
   'https://backend-projeto-integrador-2-perfumaria.onrender.com/api/v1';
 
 export { apiUrl };
-// export const apiUrl = 'http://localhost:4000/api/v1/products/productsselling';
+// export const apiUrl = 'http://localhost:4000/api/v1';
